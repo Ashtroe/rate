0d9069d3fe6b42291259a0ad27d383ac87a99bc7
Ashton <ashton@upside.health>
2023-04-17 17:25:36 -0500

    :|

--- src/pages/LoginSignup.tsx
+++ src/pages/LoginSignup.tsx
@@ -29,7 +29,7 @@ function LoginSignup({}: Props) {
   const [showSignup, setShowSignup] = useState(false)
 
   const handleSignup = () => {
-    firebase.auth().createUserWithEmailAndPassword(username, password)
+    app.auth().createUserWithEmailAndPassword(username, password)
       .then((cred) => {
         const userInfo = cred.user
         firebase.database().ref(`users/${auth.currentUser?.uid}`).
@@ -51,7 +51,7 @@ function LoginSignup({}: Props) {
   }
   const handleSignIn = () => {
     
-    firebase.auth().signInWithEmailAndPassword(username, password)
+    app.auth().signInWithEmailAndPassword(username, password)
           .then((res) => {
             if (res.user) {
               setUsername("")
